5a488c10ac322197d7807e88fb2d171121d00e68
Art Wild <wildart@gmail.com>
2019-08-17 04:02:51 -0400

    fixed IB diff calculation

--- src/ib.jl
+++ src/ib.jl
@@ -1,11 +1,18 @@
-posterior(Cidx::Int, Pidxs::Vector{Int}, mc::ClusterComplex.MahalonobisClusteringResult) =
-    Cidx ∉ Pidxs ? 0.0 : length(mc.clusters[Cidx].idx)/sum(i->length(mc.clusters[i].idx), Pidxs)
+posterior(Cidx::Int, Pidxs::Vector{Int}, mcr::ModelClusteringResult) =
+    Cidx ∉ Pidxs ? 0.0 : length(clusters(mcr)[Cidx].idx)/sum(i->length(clusters(mcr)[i].idx), Pidxs)
 
+"""
+    posterior(Cidx::Int, Pidxs::Vector{Int}, mcr::ModelClusteringResult, X::AbstractMatrix) -> Float64
+
+Calculate posterior of the clusrer `C` given the PLM cluster `P`, p(C|P).
+Clusters are given by their respective indices in the model class presented as model-base clustering 'mcr'.
+"""
 function posterior(Cidx::Int, Pidxs::Vector{Int},
-                   mc::ClusterComplex.MahalonobisClusteringResult,
+                   mcr::ModelClusteringResult,
                    X::AbstractMatrix)
-    Ppts = mapreduce(i->mc.clusters[i].idx, vcat, Pidxs)
-    mcm = mixture(ClusterComplex.clusters(mc))
+    Ppts = findall(i-> i ∈ Pidxs, assignments(mcr))
+    # Ppts = mapreduce(i->clusters(mcr)[i].idx, vcat, Pidxs)
+    mcm = MixtureModel(mcr)
     Cdist = components(mcm)[Cidx]
     pc = probs(mcm)[Cidx]
     return sum(i->
@@ -17,18 +24,18 @@ function posterior(Cidx::Int, Pidxs::Vector{Int},
 end
 
 function ibdiff(P′::Vector{Vector{Int}},
-                mc::ClusterComplex.MahalonobisClusteringResult,
+                mcr::ModelClusteringResult,
                 X::AbstractMatrix; hard::Bool=false, mstate=[Int[]])
-    k = nclusters(mc)
+    k = nclusters(mcr)
     n = size(X,2)
-    Cprior = map(c->length(c.idx)/n, clusters(mc))
+    Cprior = map(c->c/n, counts(mcr))
     ibd = 0.0
     for C in 1:k
         # print("C: $C, ")
-        p′post = hard ? posterior(C, vcat(P′...), mc) : posterior(C, vcat(P′...), mc, X)
+        p′post = hard ? posterior(C, vcat(P′...), mcr) : posterior(C, vcat(P′...), mcr, X)
         for P in P′
             # print("P: $P, ")
-            ppost = hard ? posterior(C, P, mc) : posterior(C, P, mc, X)
+            ppost = hard ? posterior(C, P, mcr) : posterior(C, P, mcr, X)
             # print("($ppost, $p′post) ")
             Dₖₗ = ppost*log(ppost/p′post)
             if ppost == 0
