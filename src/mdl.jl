25d138ea4f189c8fe67644c10491b1b7d961e744
Art Wild <wildart@gmail.com>
2020-08-17 21:05:07 -0400

    mdl: added NLL function, improved docs & minor refactoring

--- src/mdl.jl
+++ src/mdl.jl
@@ -16,16 +16,16 @@ for all points of `X`.
 regretmax(P, MC, X) = maximum(regret(P,MC,X))
 
 """
-refinedmdl(LC::Vector{MultivariateDistribution}, MC::ClusterComplex.MahalonobisClusteringResult, X::AbstractMatrix) -> (Float64, Float64, Float64)
+    refinedmdl(meta::Vector{Vector{Int}}, mcr::ModelClusteringResult, X::AbstractMatrix) -> (Float64, Float64, Float64)
 
-Calculate a refined MDL value for the model class `LC` w.r.t. to the model class 'MC'
+Calculate a refined MDL value for the metaclustering `meta` w.r.t. to the model class 'mcr'
 for all points of `X`. Returns tuple of MDL, likelihood, and complexity values.
 """
-function refinedmdl(mrg::Vector{Vector{Int}}, mcr::ModelClusteringResult, X::AbstractMatrix)
+function refinedmdl(meta::Vector{Vector{Int}}, mcr::ModelClusteringResult, X::AbstractMatrix)
     logps = hcat((logpdf(p, X) for p in models(mcr))...)
     cs = counts(mcr)
-    ps = map(i->cs[i]./sum(cs[i]), mrg)
-    logpms = mixlogpdf(logps, ps, mrg)
+    ps = map(i->cs[i]./sum(cs[i]), meta)
+    logpms = mixlogpdf(logps, ps, meta)
     return _refinedmdl(logpms)
 end
 
@@ -39,16 +39,16 @@ end
 
 
 """
-    nml(LC::Vector{MultivariateDistribution}, MC::ClusterComplex.MahalonobisClusteringResult, X::AbstractMatrix) -> (Float64, Float64, Float64)
+    nml(meta::Vector{Vector{Int}}, mcr::ModelClusteringResult, X::AbstractMatrix) -> (Float64, Float64, Float64)
 
-Calculate a normalized message length for the model class `LC` w.r.t. to the model class 'MC'
-for all points of `X`. Returns tuple of MDL, likelihood, and complexity values.
+Calculate a normalized message length for the metaclustering `meta` w.r.t. to the model class 'mcr'
+for all points of `X`. Returns tuple of NML, likelihood, and complexity values.
 """
-function nml(mrg::Vector{Vector{Int}}, mcr::ModelClusteringResult, X::AbstractMatrix)
+function nml(meta::Vector{Vector{Int}}, mcr::ModelClusteringResult, X::AbstractMatrix)
     logps = hcat((logpdf(p, X) for p in models(mcr))...)
     cs = counts(mcr)
-    ps = map(i->cs[i]./sum(cs[i]), mrg)
-    logpms = mixlogpdf(logps, ps, mrg)
+    ps = map(i->cs[i]./sum(cs[i]), meta)
+    logpms = mixlogpdf(logps, ps, meta)
     return _nml(logpms)
 end
 
@@ -62,36 +62,52 @@ end
 
 
 """
+    nll(meta::Vector{Vector{Int}}, mcr::ModelClusteringResult, X::AbstractMatrix) -> Float64
+
+Calculate a negative log-likelyhood for the metaclustering `meta` w.r.t. to the model class 'mcr'
+for all points of `X`.
+"""
+function nll(meta::Vector{Vector{Int}}, mcr::ModelClusteringResult, X::AbstractMatrix)
+    logps = hcat((logpdf(p, X) for p in models(mcr))...)
+    cs = counts(mcr)
+    ps = map(i->cs[i]./sum(cs[i]), meta)
+    logpms = mixlogpdf(logps, ps, meta)
+    minll = minimum(-logpms, dims=2)
+    return minll |> sum
+end
+
+
+"""
     mdldiff(P′::AbstractMixtureModel, MC::Vector{MultivariateDistribution}, X::AbstractMatrix) -> Float64
 
 Returns MDL difference between original and merged P′ = {Pᵢ ∪ Pⱼ} clusterings.
 """
-function mdldiff(mrg::Vector{Vector{Int}}, mcr::ModelClusteringResult, X::AbstractMatrix; kwargs...)
+function mdldiff(meta::Vector{Vector{Int}}, mcr::ModelClusteringResult, X::AbstractMatrix; kwargs...)
     logps = hcat((logpdf(p, X) for p in models(mcr))...)
-    return mdldiff(mrg, logps, assignments(mcr); kwargs...)
+    return mdldiff(meta, logps, assignments(mcr); kwargs...)
 end
 
-function mdldiff(mrg::Vector{Vector{Int}}, logps::AbstractMatrix{T},
+function mdldiff(meta::Vector{Vector{Int}}, logps::AbstractMatrix{T},
                  assign::Vector{Int}; kwargs...) where {T <: AbstractFloat}
     n = size(logps,2)
     cs = zeros(Int, n)
     for i in assign
         cs[i] += 1
     end
-    sz = map(i->sum(cs[i]), mrg)
-    ps = map(i->cs[i]./sum(cs[i]), mrg)
-    mrgidxs = vcat((findall(i-> i ∈ m, assign) for m in mrg)...)
+    sz = map(i->sum(cs[i]), meta)
+    ps = map(i->cs[i]./sum(cs[i]), meta)
+    mrgidxs = vcat((findall(i-> i ∈ m, assign) for m in meta)...)
     vlogps = view(logps, mrgidxs, :)
-    logpms = mixlogpdf(vlogps, ps, mrg)
-    return _mdldiff(logpms, vlogps, mrg, sz)
+    logpms = mixlogpdf(vlogps, ps, meta)
+    return _mdldiff(logpms, vlogps, meta, sz)
 end
 
 function _mdldiff(logP′::AbstractMatrix{T}, logPs::AbstractMatrix{T},
-                  mrg::Vector{Vector{Int}}, sz::Vector{Int}) where {T <: AbstractFloat}
+                  meta::Vector{Vector{Int}}, sz::Vector{Int}) where {T <: AbstractFloat}
     n = sum(sz)
     NLL = maximum(logP′, dims=2) |> sum
     pv = exp.(logP′)
-    s1 = max.((minimum(view(-logPs,:,i), dims=2) for i in mrg)...)
+    s1 = max.((minimum(view(-logPs,:,i), dims=2) for i in meta)...)
     s2 = -log.(sz[2]./view(pv,:,1) .+ sz[1]./view(pv,:,2))
     return NLL + (maximum(s1 .- s2) - log(n))*n  # normalize cost
 end
